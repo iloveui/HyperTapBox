1a7d71a977e3b43404ece66e2c2f9e5f7ecbb863
Ui <uimail@gmail.com>
2016-05-19 17:30:55 -0430

    TapBox

---
+++ Assets/Scripts/TapBox.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TapBox : MonoBehaviour
+{
+
+    private int pushPower = 2000;
+    public int life = 200;
+    private int selfDamage = 10;
+
+    void OnMouseDown()
+    {
+        selfDamage = this.transform.GetComponent<damageScript>().damage;
+
+        life = life - selfDamage;
+        print(life);
+        if (life < 1)
+        {
+            GetComponent<Rigidbody>().AddForce(transform.forward * pushPower);
+            this.GetComponent<Rigidbody>().useGravity = true;
+            //iTween.fadeTo(gameObject,{"alpha":0, "onComplete":"destroy"});
+            Destroy(gameObject, 5);
+            print(life);
+        }
+    }
+
+}
