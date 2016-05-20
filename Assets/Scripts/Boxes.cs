e8827e33c781bea9d6d48bb02371aa42f36be627
Francisco <groarrr@gmail.com>
2016-05-19 22:41:50 -0400

    Random Boxs

---
+++ Assets/Scripts/Boxes.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Boxes : MonoBehaviour {
+
+    public GameObject[] BoxPrefabs;
+    public void SpwnBoxs()
+    {
+       GameObject BoxSpwn = (GameObject)Instantiate(BoxPrefabs[Random.Range(0,BoxPrefabs.Length)],transform.position,Quaternion.identity);
+
+    }
+}
