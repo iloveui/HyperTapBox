d2dab8311547657d83e50ebccb2b5187d79854af
Francisco <groarrr@gmail.com>
2016-05-25 19:28:21 -0400

    no message

--- Assets/gameMaster.cs
+++ Assets/gameMaster.cs
@@ -1,14 +1,42 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class gameMaster : MonoBehaviour {
 
-    public int coins;
+    public int coins = 0;
+    public int totalCoins = 0;
     public Text coinScore;
+    private static gameMaster instance;
+    public static gameMaster Instance { get { return instance; } }
 
     void Update()
     {
-        coinScore.text = ("Coins: " + coins);
+       coinScore.text = ("Coins: " + coins);
+       Save();
+
+    }
+    public void Awake()
+    {
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+
+        if (PlayerPrefs.HasKey("Coins"))
+        {
+            //We had a previous seasion
+            coins = PlayerPrefs.GetInt("Coins");
+
+        }
+        else
+        {
+            Save();
+        }
+
+    }
+
+    public void Save()
+    {
+        PlayerPrefs.SetInt("Coins", coins);
     }
 }
