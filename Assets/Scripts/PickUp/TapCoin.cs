c41492e86a0f7ffab6d09d6829051374d74d1ca9
Ui <uimail@gmail.com>
2016-07-02 22:16:02 -0400

    Coin added

--- Assets/Scripts/PickUp/TapCoin.cs
+++ Assets/Scripts/PickUp/TapCoin.cs
@@ -5,18 +5,26 @@ public class TapCoin : MonoBehaviour {
     private int HPCoin = 1;
     private int dmgOnCoin = 1;
     private gameMaster gm;
+	public AudioSource sfx;
+	public Collider c;
 
     void Start()
     {
         gm = GameObject.FindGameObjectWithTag("GameMaster").GetComponent<gameMaster>();
+		Collider c = GetComponentInChildren<Collider>();
+		sfx = GetComponent<AudioSource>();
+
     }
 
     public void OnMouseDown()
         {
         HPCoin = HPCoin - dmgOnCoin;
+		sfx.Play();
+		gameObject.GetComponent<Animator>().Play("PickUp");
         if (HPCoin < 1)
             {
-            Destroy(gameObject);
+			c.enabled = false;
+            Destroy(gameObject, 2);
             gm.coins += 1;
             }
         }
