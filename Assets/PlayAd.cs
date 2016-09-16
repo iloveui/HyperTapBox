aa05dd11a76956cc1abf3c3b0bb7fbccf23d6a38
Ui <uimail@gmail.com>
2016-09-15 20:42:20 -0400

    Update

---
+++ Assets/PlayAd.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.Advertisements;
+using System.Collections;
+
+public class PlayAd : MonoBehaviour {
+
+	public void ShowAd (){
+		if (Advertisement.IsReady ()) {
+			Advertisement.Show("rewardedVideo", new ShowOptions() {resultCallback = HandleAdResult});
+		}
+	}
+
+	private void HandleAdResult (ShowResult results){
+	
+		switch (results) {
+		case ShowResult.Finished:
+			Debug.Log ("le dimos las lucas");
+			break;
+		case ShowResult.Skipped:
+			Debug.Log ("se hace el rata");
+			break;
+		case ShowResult.Failed:
+			Debug.Log ("se le cayo el internet");
+			break;
+		}
+	}
+
+}
